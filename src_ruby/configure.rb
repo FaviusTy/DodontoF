1ed409874af6384e53d0c7fb157226d6abc0ea0c
Favi_ty <favius983@gmail.com>
2013-03-07 12:50:58 +0900

    configure.rbとsample用のymlファイルを追加

--- src_ruby/configure.rb
+++ src_ruby/configure.rb
@@ -10,9 +10,30 @@ class Configure
   def self.method_missing(method, *args)
     @@base.send(method, *args)
   end
+
+  def self.version
+    "#{version_only}(#{version_date})"
+  end
+
+  #ログアウトと判定される応答途絶時間(秒)
+  #下記秒数以上ブラウザから応答が無い場合はログアウトしたと判定。
+  def self.login_timeout
+    refresh_timeout * 1.5 + 10
+  end
+
+  # ログイン状況を記録するファイル
+  # TODO:FIXME これはsaveDirInfoに定義すべき
+  def self.login_count_file
+    File.join(save_data_dir, 'saveData', 'loginCount.txt')
+  end
 end
 
 if $0 === __FILE__
-  puts "version: #{Configure.Version.numbering}"
+  puts "version: #{Configure.version}"
+  puts "refresh_timeout: #{Configure.refresh_timeout}:#{Configure.refresh_timeout.class}"
+  puts "login_timeout: #{Configure.login_timeout}:#{Configure.login_timeout.class}"
+  puts "login_count_file: #{Configure.login_count_file}"
+  puts "save_data_lock_file_dir: #{Configure.save_data_lock_file_dir}:#{Configure.save_data_lock_file_dir.class}"
+  puts "dicebot_order: #{Configure.dicebot_order}"
   puts "undefined: #{Configure.undefined}"
 end

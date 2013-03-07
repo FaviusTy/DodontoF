20b13e843d1527347e74b5228ec38c88f1b20f08
Favi_ty <favius983@gmail.com>
2013-03-07 11:36:57 +0900

    configure.rbとsample用のymlファイルを追加

---
+++ src_ruby/configure.rb
@@ -0,0 +1,17 @@
+# encoding:utf-8
+
+require 'yaml'
+require './n_ostruct'
+
+class Configure
+  DEFAULT_FILE_PATH = 'settings.yml'
+  @@base ||= NestedOpenStruct.new(YAML.load_file(DEFAULT_FILE_PATH))
+
+  def self.method_missing(method, *args)
+    @@base.send(method, *args)
+  end
+end
+
+if $0 === __FILE__
+  puts "version: #{Configure.Version.numbering}"
+end

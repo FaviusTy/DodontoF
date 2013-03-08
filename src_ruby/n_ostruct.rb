56cf4f393b235ce0c2ccdcceb93ccf7194b96804
Favi_ty <favius983@gmail.com>
2013-03-09 00:21:07 +0900

    n_ostruct改修
    親クラスの定義メソッドのうち、再実装が必要なものを追加定義
    Enumerableをincludeするため、eachメソッドを追加定義

--- src_ruby/n_ostruct.rb
+++ src_ruby/n_ostruct.rb
@@ -3,6 +3,7 @@ require 'ostruct'
 require './nested_hash'
 
 class NestedOpenStruct < OpenStruct
+  include Enumerable
 
   # コンストラクタ.
   # @param [Hash] base_hash 設定する要素とその値になるHashオブジェクト
@@ -18,10 +19,20 @@ class NestedOpenStruct < OpenStruct
 
   # 自身に設定されている要素とその値をHashオブジェクトに変換して返します.
   # 値にNestedOpenStructインスタンスが含まれている場合は、再帰的にHashオブジェクトに変換されます
-  def to_hash
+  def marshal_dump
     result = @table.dup
     result.each do |key, value|
-      result[key] = value.instance_of?(NestedOpenStruct) ? value.to_hash : value
+      result[key] = value.instance_of?(NestedOpenStruct) ? value.marshal_dump : value
+    end
+  end
+
+  # 引数xのHashを元に@tableを再構築します.
+  # 事実上、インスタンスを再生成する事を意味します
+  def marshal_load(x)
+    @table = {}
+    x.each do |key,value|
+      @table[:"#{key}"] = value.instance_of? Hash ? NestedOpenStruct.new(value) : value
+      new_ostruct_member(key)
     end
   end
 
@@ -29,9 +40,9 @@ class NestedOpenStruct < OpenStruct
   def merge!(add_obj, nil_clear = false)
     case add_obj
       when Hash
-        @table = self.to_hash.deep_merge!(NestedOpenStruct.new(add_obj).to_hash, nil_clear)
+        @table = self.marshal_dump.deep_merge!(NestedOpenStruct.new(add_obj).marshal_dump, nil_clear)
       when NestedOpenStruct
-        @table = self.to_hash.deep_merge!(add_obj.to_hash, nil_clear)
+        @table = self.marshal_dump.deep_merge!(add_obj.marshal_dump, nil_clear)
       else
         # nothing
     end
@@ -39,9 +50,16 @@ class NestedOpenStruct < OpenStruct
     self
   end
 
-  # TODO 要実装. fieldとvalueを返す
+  # @tableのeach処理を実施します
   def each
+    @table.each{|key ,value| yield(key, value)}
+  end
 
+  # オブジェクト比較演算子のオーバーロード.
+  # otherがNestedOpenStructであり、otherのtableが自身の@tableと等価である時に真と見なします
+  def ==(other)
+    return false unless(other.kind_of?(NestedOpenStruct))
+    return @table == other.table
   end
 end
 
@@ -56,9 +74,10 @@ if $0 === __FILE__
   puts "undefine: #{n_ostruct.key4}:#{n_ostruct.key4.class}"
   puts "n_key1: #{n_ostruct.key3.n_key1}"
   puts "inspect: #{n_ostruct.inspect}"
-  puts "to_hash: #{n_ostruct.to_hash}"
-  puts "merge!(in Hash): #{n_ostruct.merge!({"add_key1" => "add!"}).to_hash}"
+  puts "to_hash: #{n_ostruct.marshal_dump}"
+  puts "merge!(in Hash): #{n_ostruct.merge!({"add_key1" => "add!"}).marshal_dump}"
   add_n_ostruct = NestedOpenStruct.new({"key3" => {n_key2: 'add_nested_value'}})
-  puts "merge!(in NestedOpenStruct): #{n_ostruct.merge!(add_n_ostruct).to_hash}"
-  puts "merge!(in illegal instance): #{n_ostruct.merge!('other').to_hash}"
+  puts "merge!(in NestedOpenStruct): #{n_ostruct.merge!(add_n_ostruct).marshal_dump}"
+  puts "merge!(in illegal instance): #{n_ostruct.merge!('other').marshal_dump}"
+  puts "map: #{n_ostruct.map{|_,v| p "#{v}" }}"
 end
